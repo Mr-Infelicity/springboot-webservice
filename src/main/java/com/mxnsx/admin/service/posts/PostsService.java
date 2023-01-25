601bdbd9eb84e34f9ddc0b16a9fab39e40e7d5df
dopox <dopoxx@naver.com>
2023-01-25 20:03:42 +0900

    web

--- src/main/java/com/mxnsx/admin/service/posts/PostsService.java
+++ src/main/java/com/mxnsx/admin/service/posts/PostsService.java
@@ -1,11 +1,18 @@
 package com.mxnsx.admin.service.posts;
 
+import com.mxnsx.admin.domain.posts.Posts;
 import com.mxnsx.admin.domain.posts.PostsRepository;
+import com.mxnsx.admin.web.dto.PostsListResponseDto;
+import com.mxnsx.admin.web.dto.PostsResponseDto;
 import com.mxnsx.admin.web.dto.PostsSaveRequestDto;
 
+import com.mxnsx.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +22,35 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
